5446908ee14e9b0d7c690d2cecbe6992f7fe922b
Andrew Eaves <andrew@eavesofit.com>
2020-07-17 09:37:16 -0400

    Installed the cert for windows 10 Fixed environment variable loading

--- nicedice.py
+++ nicedice.py
@@ -1,10 +1,12 @@
 # bot.py
 import os
-
 import discord
+from os.path import join, dirname
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()

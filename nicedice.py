580910e631b120211b05d82adcca4fc6536537c3
Andrew Eaves <andrew@eavesofit.com>
2020-07-17 11:25:07 -0400

    Got the on_message working

--- nicedice.py
+++ nicedice.py
@@ -8,11 +8,14 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+async def on_message(message):
+    if message.content.startswith('/roll'):
+        channel = message.channel
+        await channel.send('Roll dem dice!')
 
 client.run(TOKEN)

6d6ffbf54d272291a68dcf427096b016f63e8e50
Andrew Eaves <andrew@eavesofit.com>
2020-07-17 13:50:09 -0400

    Working with added static number

--- nicedice.py
+++ nicedice.py
@@ -1,5 +1,7 @@
 # bot.py
 import os
+import re
+import dice
 import discord
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -16,6 +18,29 @@ client = discord.Client()
 async def on_message(message):
     if message.content.startswith('/roll'):
         channel = message.channel
-        await channel.send('Roll dem dice!')
+        await channel.send('Rolling dem dice!')
 
+
+        if "+" in message.content or "-" in message.content:
+            messageoper = re.search("[\-\+]", message.content.split("/roll")[1])
+            messagesplit = (message.content.split("/roll")[1]).split("+")
+            print("messagesplit")
+            print(messagesplit)
+            droll = dice.roll(messagesplit[0])
+            if isinstance(droll, list):
+                drollsum = sum(droll)
+                drollresult = drollsum
+            else:
+                drollresult = droll
+            if messageoper.group() == "-":
+                drollsum = drollresult - int(messagesplit[1])
+            else:
+                drollsum = drollresult + int(messagesplit[1])
+
+            drollresult = "You rolled " + str(droll) + " " + str(messageoper.group()) + " " + messagesplit[1] + " = " + str(drollsum)
+
+        print(drollresult)
+
+        await channel.send(drollresult)
+        
 client.run(TOKEN)

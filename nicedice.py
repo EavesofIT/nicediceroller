b0b8e4302592f5fd54ad748089f73ef317f3c3f1
AJ Eaves <andrew@eavesofit.com>
2020-07-19 21:19:11 -0400

    Added the help method.

--- nicedice.py
+++ nicedice.py
@@ -20,6 +20,10 @@ async def on_message(message):
         channel = message.channel
         await channel.send('Rolling dem dice!')
 
+        if "help" in message.content or "-h" in message.content:
+            help = dice_help()
+            await channel.send(help) 
+
         messageoper = None
 
         if "+" in message.content or "-" in message.content: 
@@ -27,16 +31,20 @@ async def on_message(message):
             messagesplit = (message.content.split("/roll")[1]).split("+")[1]
         else:
             messagesplit = (message.content.split("/roll")[1])
+
         droll = dice.roll(messagesplit)
+
         if isinstance(droll, list):
             drollsum = sum(droll)
             drollresult = drollsum
         else:
             drollresult = droll
+            
         if "-" in message.content:
             drollsum = drollresult - int(messagesplit[1])
         elif "+" in message.content:
             drollsum = drollresult + int(messagesplit[1])
+
         if "+" in message.content or "-" in message.content:
             drollresult = "You rolled " + str(droll) + " " + str(messageoper.group()) + " " + messagesplit[1] + " = " + str(drollsum)
         else:
@@ -44,10 +52,17 @@ async def on_message(message):
 
         await channel.send(drollresult)
 
-def dice_roll_multi(self, arr_dicerolls):
+def dice_help():
+    help = "/roll can be used to roll multiple many styles of roll such as 1d6, 1d6+2, DL, body part, etc"
+    return help
+
+def dice_roll(self, arr_dicerolls):
     pass
 
 def dice_roll_basic(self, diceroll):
+    pass
+
+def dice_roll_dl():
     pass
 
 def sanitize_input(self, inputs):
